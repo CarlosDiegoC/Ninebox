e8001f5daab34d4d664bb680fdafeb7ad1717a5b
pans <priscilla.santos@gft.com>
2022-10-13 14:01:25 -0300

    link azure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -48,6 +48,7 @@ const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigato
         clientId: '8041545d-1c84-4b1b-9ff3-39b0e8076cf2',
         authority: 'https://login.microsoftonline.com/d28ce3ba-a6d5-4bd9-8718-290cb947a6c4',
         redirectUri: 'http://localhost:4200',
+        /* redirectUri: 'https://appnineboxhermesdev.azurewebsites.net/', */
       },
       cache: {
         cacheLocation: 'localStorage',
